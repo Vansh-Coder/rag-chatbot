c4f7bd311be8e721d44ecb2d34a986d7517b214b
Vansh-Coder <guptavansh311@gmail.com>
2025-05-27 02:44:03 +0530

    Completing HomePage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,39 @@
+import React from "react";
+import { cn } from "@/lib/utils";
+import { Spotlight } from "@/components/ui/spotlight";
+import MovingBorderButton from "@/components/ui/movingBorderButton";
+
 export default function Home() {
   return (
-    <main>
-      Home Page
-    </main>
+    <div className="relative z-0">
+      {/* Fake background that covers over-scroll areas */}
+      <div className="fixed inset-0 -z-10 bg-black" />
+      <div className="fixed bg-black" />
+      <div
+        className="relative flex h-screen w-full overflow-hidden bg-black/[0.96] antialiased md:items-center md:justify-center">
+        <div
+          className={cn(
+            "pointer-events-none absolute inset-0 [background-size:40px_40px] select-none",
+            "[background-image:linear-gradient(to_right,#171717_1px,transparent_1px),linear-gradient(to_bottom,#171717_1px,transparent_1px)]"
+          )} />
+        <Spotlight className="-top-40 left-0 md:-top-20 md:left-60" fill="white" />
+        <div className="relative z-10 mx-auto w-full max-w-7xl p-4 pt-20 md:pt-0">
+          <h1
+            className="bg-opacity-50 bg-gradient-to-b from-neutral-50 to-neutral-400 bg-clip-text text-center text-4xl font-bold text-transparent md:text-7xl">
+            EduBot <br /> Your AI ChatBot for Study.
+          </h1>
+          <p
+            className="mx-auto mt-4 max-w-lg text-center text-base font-normal text-neutral-300">
+            EduBot is a great way to chat with your course material, literally!
+            Simply upload your study documents and ask the chatbot any questions. 
+            Start your study journey with EduBot now!
+          </p>
+          <div className="mt-6 flex justify-center">
+            <MovingBorderButton label="Login" path="/login" />
+            <MovingBorderButton label="Try it first!" path="/try" />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
