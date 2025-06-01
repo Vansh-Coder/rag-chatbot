e57dffd292b6a7543e7c997760a9dc96d5a213fa
Vansh-Coder <guptavansh311@gmail.com>
2025-06-01 07:16:59 +0530

    Adding insights to examine user experience

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -1,13 +1,18 @@
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata = {
-  title: "RAG Project",
+  title: "EduBot: AI ChatBot",
+  description: "EduBot makes studying documents easy and fun!",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-black">{children}</body>
+      <body className="bg-black">
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
