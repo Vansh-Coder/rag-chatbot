8d2bb9daeedc16cfbd30b77db3d80aa79816959b
Vansh-Coder <guptavansh311@gmail.com>
2025-06-01 07:21:10 +0530

    Adding user analytics

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata = {
@@ -11,6 +12,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className="bg-black">
         {children}
+        <Analytics />
         <SpeedInsights />
       </body>
     </html>
